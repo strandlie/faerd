5970cfee58c0750487a7959fcc07c6e55b2a028c
Håkon Strandlie <hstrandlie@gmail.com>
2019-06-20 21:58:30 +0200

    Initial Commit

---
+++ BusTime WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  BusTime WatchKit Extension
+//
+//  Created by Håkon Strandlie on 20/06/2019.
+//  Copyright © 2019 Håkon Strandlie. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController : WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ BusTime WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  BusTime WatchKit Extension
+//
+//  Created by Håkon Strandlie on 20/06/2019.
+//  Copyright © 2019 Håkon Strandlie. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews : PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
