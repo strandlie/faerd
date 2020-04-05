87d9e740443c2e0361eeebf8d1e1fec5f61b9d0a
Håkon Strandlie <haakstr@stud.ntnu.no>
2020-04-05 20:35:01 +0200

    Add basic setup for iOS app

---
+++ FerdiOS/ContentView.swift
@@ -0,0 +1,82 @@
+//
+//  ContentView.swift
+//  FerdiOS
+//
+//  Created by Håkon Strandlie on 28/03/2020.
+//  Copyright © 2020 Håkon Strandlie. All rights reserved.
+//
+
+import SwiftUI
+
+private let dateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = .medium
+    dateFormatter.timeStyle = .medium
+    return dateFormatter
+}()
+
+struct ContentView: View {
+    @State private var dates = [Date]()
+
+    var body: some View {
+        /*
+        NavigationView {
+            MasterView(dates: $dates)
+                .navigationBarTitle(Text("Master"))
+                .navigationBarItems(
+                    leading: EditButton(),
+                    trailing: Button(
+                        action: {
+                            withAnimation { self.dates.insert(Date(), at: 0) }
+                        }
+                    ) {
+                        Image(systemName: "plus")
+                    }
+                )
+            DetailView()
+        }.navigationViewStyle(DoubleColumnNavigationViewStyle())
+        */
+        NavigationView {
+            StopsListView()
+        }.navigationViewStyle(DoubleColumnNavigationViewStyle())
+    }
+}
+
+struct MasterView: View {
+    @Binding var dates: [Date]
+
+    var body: some View {
+        List {
+            ForEach(dates, id: \.self) { date in
+                NavigationLink(
+                    destination: DetailView(selectedDate: date)
+                ) {
+                    Text("\(date, formatter: dateFormatter)")
+                }
+            }.onDelete { indices in
+                indices.forEach { self.dates.remove(at: $0) }
+            }
+        }
+    }
+}
+
+struct DetailView: View {
+    var selectedDate: Date?
+
+    var body: some View {
+        Group {
+            if selectedDate != nil {
+                Text("\(selectedDate!, formatter: dateFormatter)")
+            } else {
+                Text("Detail view content goes here")
+            }
+        }.navigationBarTitle(Text("Detail"))
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FerdiOS/Views/StopsListView.swift
@@ -0,0 +1,21 @@
+//
+//  StopsListView.swift
+//  FerdiOS
+//
+//  Created by Håkon Strandlie on 28/03/2020.
+//  Copyright © 2020 Håkon Strandlie. All rights reserved.
+//
+
+import SwiftUI
+
+struct StopsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StopsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        StopsListView()
+    }
+}
