7970ad730eac25cac62d15ba60bf0e90b21411a4
Håkon Strandlie <haakstr@stud.ntnu.no>
2020-04-07 13:50:47 +0200

    Removed debug statement

--- Ferd WatchKit Extension/HostingController.swift
+++ Ferd WatchKit Extension/HostingController.swift
@@ -13,11 +13,6 @@ import SwiftUI
 class HostingController : WKHostingController<ContentView> {
     
     override var body: ContentView {
-        /*
-        #if DEBUG
-        StoreController.shared.setProductStatus(false, for: UserDefaultsKeys.premiumFavoritesStatus.rawValue)
-        #endif
-        */
         LocationController.shared.enableBasicLocationServices()
         return ContentView()
     }
